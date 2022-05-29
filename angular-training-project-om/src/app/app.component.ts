8e015d8c4be1c1a29911f0f8a53ce7daac95a8e1
Omar Qasirov <omarbmohamad@gmail.com>
2022-05-29 12:00:57 -0500

    Adding SalesPerson and table

--- angular-training-project-om/src/app/app.component.ts
+++ angular-training-project-om/src/app/app.component.ts
@@ -8,6 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Another Angular Review Project';
 
-  firstName = "Charles";
-  lastName ="Kory";
+  
 }
---
+++ angular-training-project-om/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  constructor() { }
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup","Kumar","anup.kumar@luv2code.com", 50000),
+    new SalesPerson("Quark","Son of Keldar","quark@quarks.biz",60000),
+    new SalesPerson("Rom","Son of Keldar","rom@quarks.biz",40000),
+    new SalesPerson("Morn","Norm", "morn@quarks.biz", 45000)
+  ];
+
+  ngOnInit(): void {
+  }
+
+}
